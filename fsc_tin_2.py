5033eb8994ef7367b17eca9e533d623abac65d2e
Trần Ngọc Minh Thông <thongtnmfct31178@gmail.com>
2025-01-14 07:46:18 +0700

    Add files via upload

--- fsc_tin_2.py
+++ fsc_tin_2.py
@@ -1,3 +1,7 @@
 #in ra màn hình 10 dòng Hello World
-for _ in range(1000):
+for _ in range(10):
     print("Hello World")
+
+#for "bienchay" in range (m,n)
+    #bienchay: Biến quản lý số lần lặp
+    #(m,n): Danh sách giá trị lặp
